76752752c336c398b653a6639e6b3fd72df2061a
dmmscript <daniellymoro12@gmail.com>
2025-02-09 12:52:34 -0300

    fix: filter dogs with valid breed

--- src/app/services-dog.service.ts
+++ src/app/services-dog.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -9,11 +10,20 @@ import { environment } from 'src/environments/environment';
 export class ServicesDogService {
   private apiUrl = environment.apiUrl;
   private apiKey = environment.apiKey;
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-  
-  getDogs(): Observable<any[]> {
+  getDogs(): Observable<Dog[]> {
     const headers = new HttpHeaders().set('x-api-key', this.apiKey);
-    return this.http.get<any[]>(this.apiUrl, { headers });
+    const params = new HttpParams().set('has_breeds', '1');
+    return this.http.get<any[]>(this.apiUrl, { headers, params }).pipe(
+      map((dogs: any[]) =>
+        dogs.filter((dog: Dog) => dog.breed !== 'Unknown Breed' && dog.url !== null)
+      )
+    );
   }
+}
+
+interface Dog {
+  breed: string;
+  url: string | null;
 }
