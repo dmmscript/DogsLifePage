92c72367671db26fdfccbb3c185e5f06baf0108a
dmmscript <daniellymoro12@gmail.com>
2025-02-10 22:30:56 -0300

    feat: custom layout and add breeds

--- src/app/components/dogs-list/dogs-list.component.ts
+++ src/app/components/dogs-list/dogs-list.component.ts
@@ -1,19 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { ServicesDogService } from 'src/app/services-dog.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dogs-list',
   templateUrl: './dogs-list.component.html',
   styleUrls: ['./dogs-list.component.scss']
 })
-export class DogsListComponent implements OnInit{
-  dogs: any[] = [];
+  export class DogsListComponent implements OnInit{
+    dogs: any[] = [];
+    filteredDogs: any[] = [];
+    breeds: string[] = [];
+    searchQuery: string = '';
+    selectedBreed: string = '';
   
-  constructor(private dogService: ServicesDogService) { }
+  constructor(private dogService: ServicesDogService, private router: Router) { }
   
   ngOnInit(): void {
-    this.dogService.getDogs().subscribe(data => {
-      this.dogs = data;
+    this.loadDogs();
+    this.loadBreeds();
+  }
+
+  loadDogs(): void {
+    this.dogService.getDogs().subscribe(images => {
+      this.dogs = images;
+      this.filteredDogs = images;
     });
+  }
+
+  loadBreeds(): void {
+    this.dogService.getBreeds().subscribe(breeds => {
+      this.breeds = breeds.map(breed => breed.name);
+    });
+  }
+
+  searchDogs(): void {
+    if (this.searchQuery.trim() || this.selectedBreed) {
+      this.filteredDogs = this.dogs.filter(dog => 
+        (dog.breeds[0]?.name.toLowerCase().includes(this.searchQuery.toLowerCase()) || 
+         dog.temperament?.toLowerCase().includes(this.searchQuery.toLowerCase())) &&
+        (this.selectedBreed ? dog.breeds[0]?.name === this.selectedBreed : true)
+      );
+    } else {
+      this.filteredDogs = this.dogs;
+    }
+  }
+
+  onBreedSelected(breed: string): void {
+    this.router.navigate(['/dog-breed', breed]);
+  }
+
+  clearBreedFilter(): void {
+    this.selectedBreed = '';
+    this.searchDogs();
   }
 }
